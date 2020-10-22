fd0245877c96fad61f25bc16cd2c77ab2d5d4842
VuVanHoc <hocvanvu1999@gmail.com>
2020-10-22 23:26:53 +0700

    build

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { Button, DatePicker } from "antd";
-
+import { Button, DatePicker, Input } from "antd";
+import Modal from "antd/lib/modal/Modal";
+import TextArea from "antd/lib/input/TextArea";
+var forge = require("node-forge");
 function App() {
+  const [visible, setVisible] = useState(false);
+  const [sending, setSending] = useState(false);
+  const handleCloseModal = () => {
+    setVisible(false);
+  };
+  const onOk = () => {
+    setSending(true);
+    setTimeout(() => {
+      setVisible(false);
+      setSending(false);
+
+    }, 5000);
+  };
   return (
     <div>
-      <h2>HIhiihi</h2>
-      <Button type="dashed">Click me</Button>
+      <Button type="dashed" onClick={() => setVisible(true)}>
+        Click me
+      </Button>
       <DatePicker format="DD/MM/YYYY" />
+      <Modal
+        title="Title modal"
+        maskClosable={false}
+        visible={visible}
+        onOk={onOk}
+        onCancel={handleCloseModal}
+        confirmLoading={sending}
+      >
+        <TextArea rows={5} />
+      </Modal>
     </div>
   );
 }
